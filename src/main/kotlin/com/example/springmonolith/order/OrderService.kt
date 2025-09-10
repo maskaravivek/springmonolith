bf7b701d390b62758b1be0817418bd6edc8e994b
Vivek Kumar Maskara <maskaravivek@gmail.com>
2025-09-09 22:27:33 -0700

    Minor

--- src/main/kotlin/com/example/springmonolith/order/OrderService.kt
+++ src/main/kotlin/com/example/springmonolith/order/OrderService.kt
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service
 class OrderService {
 
     fun getGreeting(): String {
-        return "Hello from Order Module! 🛒"
+        return "Hello from Order Module!"
     }
 }
